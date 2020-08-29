40973b902fc0a8b234787c55698f12969c54e500
Shaon <shaon63@gmail.com>
2020-08-29 11:55:00 +0600

    refactor django admin

--- boards/admin.py
+++ boards/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from boards.models import Board, Topic, Post
 
-admin.site.register(Board)
-admin.site.register(Topic)
-admin.site.register(Post)
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    list_filter = ('name',)
+    list_per_page = 15
+    fields = ('name', 'description')
+    search_fields = ('id', 'name')
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subject', 'board', 'views', 'starter', 'last_updated')
+    list_per_page = 15
+    search_fields = ('id', 'subject', 'board', 'starter')
+    fields = ('subject', 'board',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'message', 'topic', 'created_at')
+    list_per_page = 15
+    fields = ('message', 'topic')
+    search_fields = ('id', 'topic', 'message')
+
+
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Post, PostAdmin)
